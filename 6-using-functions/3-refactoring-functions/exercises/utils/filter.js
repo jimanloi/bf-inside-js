96b3ca085188af6bbec9241bdd7c7ba6515a665a
jimanloi <jimanloi@hotmail.com>
2025-03-19 17:00:10 +0100

    finished exercises until 6-3.

--- 6-using-functions/3-refactoring-functions/exercises/utils/filter.js
+++ 6-using-functions/3-refactoring-functions/exercises/utils/filter.js
@@ -1,4 +1,17 @@
 /**
- *
+ *filter out specific characters inside a string
+ @param {string} (toFilter='') - the string to filter
+ @param (string) (removeThese='') - the characters to remove
+ @return (string) filtered string
  */
-export const filter = (toFilter, removeThese) => {};
+export const filter = (toFilter = '', removeThese = '') => {
+  let newText = '';
+  toFilter = toFilter.toLowerCase();
+  removeThese = removeThese.toLowerCase();
+  for (const char of toFilter) {
+    if (!removeThese.includes(char)) {
+      newText += char;
+    }
+  }
+  return newText;
+};
--- 6-using-functions/3-refactoring-functions/exercises/utils/shift-characters.js
+++ 6-using-functions/3-refactoring-functions/exercises/utils/shift-characters.js
@@ -1,4 +1,12 @@
 /**
  *
  */
-export const shiftCharacters = (text, shift) => {};
+export const shiftCharacters = (text = '', shift = 0) => {
+  let newText = '';
+  for (let i = 0; i < text.length; i++) {
+    const newCode = text.charCodeAt(i) + shift;
+    const newChar = String.fromCharCode(newCode);
+    newText += newChar
+  }
+  return newText;
+};
